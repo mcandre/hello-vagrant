eae540a7a4ab724ecdd69f5e9a8c46cfaa1480e9
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-02-08 23:39:06 -0500

    lint

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,45 +2,45 @@
 
 ## Update apt before installing any packages
 
-exec { "apt-update":
-  command => "/usr/bin/apt-get update"
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update'
 }
 
-Exec["apt-update"] -> Package <| |>
+Exec['apt-update'] -> Package <| |>
 
 ## Link bash profile
 
-file { "/home/vagrant/.bash_profile":
+file { '/home/vagrant/.bash_profile':
   ensure => link,
-  target => "/vagrant/.bash_profile"
+  target => '/vagrant/.bash_profile'
 }
 
 # Development
 
-package { ["build-essential", "tree", "splint"]:
+package { ['build-essential', 'tree', 'splint']:
   ensure => latest
 }
 
 # Testing
 
-package { "rubygems":
+package { 'rubygems':
   ensure => latest
 }
 
-package { "bundler":
-  ensure => latest,
-  provider => "gem",
-  require => Package[[rubygems]]
+package { 'bundler':
+  ensure   => latest,
+  provider => 'gem',
+  require  => Package[[rubygems]]
 }
 
-exec { "bundle":
-  command => "/usr/local/bin/bundle",
-  cwd => "/vagrant/",
+exec { 'bundle':
+  command   => '/usr/local/bin/bundle',
+  cwd       => '/vagrant/',
   subscribe => Package[[bundler]]
 }
 
 # Ops
 
-package { "vim":
+package { 'vim':
   ensure => latest
 }
